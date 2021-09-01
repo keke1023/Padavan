ec1075549d9dc93f5b420da15cad0dfdc33586f7
zuols <zuoliansheng123@gmail.com>
2021-09-01 22:32:38 +0800

    add BELL-A-040W-Q

---
+++ trunk/configs/boards/BELL-A-040W-Q/board.h
@@ -0,0 +1,35 @@
+/* BELL-A-040W-Q */
+
+#define BOARD_PID		"BELL-A-040W-Q"
+#define BOARD_NAME		"BELL-A-040W-Q"
+#define BOARD_DESC		"nokia-sbell A-040W-Q Wireless Router"
+#define BOARD_VENDOR_NAME	"nokia-sbell"
+#define BOARD_VENDOR_URL	"http://www.nokia-sbell.com/"
+#define BOARD_MODEL_URL		"http://www.nokia-sbell.com/"
+#define BOARD_BOOT_TIME		30
+#define BOARD_FLASH_TIME	120
+#define BOARD_GPIO_BTN_FN1   7
+#define BOARD_GPIO_BTN_RESET 15
+#define BOARD_GPIO_BTN_WPS   18
+#undef  BOARD_GPIO_LED_ALL
+#undef  BOARD_GPIO_LED_WIFI
+#define  BOARD_GPIO_LED_SW2G   4
+#define  BOARD_GPIO_LED_SW5G  3
+#undef  BOARD_GPIO_LED_POWER
+#undef  BOARD_GPIO_LED_LAN
+#define BOARD_GPIO_LED_WAN	15
+#define BOARD_GPIO_LED_WPS
+#define BOARD_GPIO_LED_USB  13
+#undef BOARD_GPIO_LED_ROUTER
+#undef BOARD_GPIO_PWR_USB_ON
+#undef BOARD_GPIO_PWR_USB
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX	2
+#define BOARD_NUM_ANT_5G_RX	2
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	1
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
+#undef BOARD_NUM_UPHY_USB3
+#define BOARD_USB_PORT_SWAP	1
