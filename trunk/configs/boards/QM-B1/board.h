7a80955188762183d882a483a9fc3f16a12b40cb
vb1980 <vb1980@sina.com>
2021-12-18 18:16:55 +0800

    add QM-B1 support

---
+++ trunk/configs/boards/QM-B1/board.h
@@ -0,0 +1,36 @@
+/* QM-B1 */
+
+#define BOARD_PID		"QM-B1"
+#define BOARD_NAME		"QM-B1"
+#define BOARD_DESC		"Qianmo Wireless Router"
+#define BOARD_VENDOR_NAME	"Qianmo Technology"
+#define BOARD_VENDOR_URL	"https://www.qianmo.com/"
+#define BOARD_MODEL_URL		"https://www.qianmo.com/"
+#define BOARD_BOOT_TIME		30
+#define BOARD_FLASH_TIME	120
+#undef BOARD_GPIO_BTN_FN1
+#define BOARD_GPIO_BTN_RESET	18
+#undef BOARD_GPIO_BTN_WP
+#undef BOARD_GPIO_LED_ALL
+#undef BOARD_GPIO_LED_WIFI
+#define BOARD_GPIO_LED_SW2G	13
+#define BOARD_GPIO_LED_SW5G	17
+#define BOARD_GPIO_LED_POWER 15
+#undef BOARD_GPIO_LED_SYS
+#undef BOARD_GPIO_LED_LAN
+#undef BOARD_GPIO_LED_WAN
+#define BOARD_GPIO_LED_USB 14
+#undef BOARD_GPIO_LED_ROUTER
+#define BOARD_GPIO_PWR_USB 16
+#define BOARD_GPIO_PWR_USB_ON 1
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX	2
+#define BOARD_NUM_ANT_5G_RX	2
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	1
+#define BOARD_NUM_ETH_EPHY	2
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
+#define BOARD_NUM_UPHY_USB3	1
+#define BOARD_USB_PORT_SWAP	1
