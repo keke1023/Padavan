e3026bcd8b9406345053e7eb8c57dc8af2f1d9e0
vb1980 <vb1980@sina.com>
2022-03-13 22:07:52 +0800

    Add RTL8367S support & Mercury D12G config

---
+++ trunk/configs/boards/D12G/board.h
@@ -0,0 +1,29 @@
+/* D12G */
+
+#define BOARD_PID		"D12G"
+#define BOARD_NAME		"D12G"
+#define BOARD_DESC		"MWECURY D12G Wireless Router"
+#define BOARD_VENDOR_NAME	"MWECURY"
+#define BOARD_VENDOR_URL	"http://www.MWECURY.com/"
+#define BOARD_MODEL_URL		"http://www.MWECURY.com/"
+#define BOARD_BOOT_TIME		25
+#define BOARD_FLASH_TIME	120
+#define BOARD_GPIO_BTN_RESET	13
+#undef  BOARD_GPIO_BTN_WPS
+#undef  BOARD_GPIO_LED_ALL
+#undef  BOARD_GPIO_LED_WIFI	
+#define BOARD_GPIO_LED_POWER    42  	/* hw led */
+#undef  BOARD_GPIO_LED_LAN
+#undef  BOARD_GPIO_LED_WAN
+#undef  BOARD_GPIO_LED_USB
+#undef  BOARD_GPIO_PWR_USB
+#undef  BOARD_GPIO_LED_ROUTER	
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX	2
+#define BOARD_NUM_ANT_5G_RX	2
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	2
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
+
