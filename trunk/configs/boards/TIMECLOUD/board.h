8cef4c5ee20f1b70aaed8053431a0e34e5997ae2
vb1980 <vb1980@sina.com>
2022-06-26 19:02:14 +0800

    add timecloud support

---
+++ trunk/configs/boards/TIMECLOUD/board.h
@@ -0,0 +1,35 @@
+/* thunder timecloud */
+
+#define BOARD_PID		"TIMECLOUD"
+#define BOARD_NAME		"TIMECLOUD"
+#define BOARD_DESC		"thunder timeCloud"
+#define BOARD_VENDOR_NAME	"Xunlei(Shenzheng) Limited"
+#define BOARD_VENDOR_URL	"https://www.xunlei.com/"
+#define BOARD_MODEL_URL	"http://xzb.xunlei.com/"
+#define BOARD_BOOT_TIME	30
+#define BOARD_FLASH_TIME	120
+#define BOARD_GPIO_BTN_RESET	13
+#define BOARD_GPIO_BTN_WPS	4
+#define BOARD_GPIO_SWICH_SD	43
+#undef  BOARD_GPIO_LED_ALL
+#undef  BOARD_GPIO_LED_WIFI
+#undef  BOARD_GPIO_LED_SW2G		/* soft led */
+#undef  BOARD_GPIO_LED_SW5G		/* soft led */
+#undef  BOARD_GPIO_LED_POWER
+#undef  BOARD_GPIO_LED_LAN
+#undef  BOARD_GPIO_LED_WAN
+#define BOARD_GPIO_LED_USB	7
+#define BOARD_GPIO_LED_ROUTER	8
+#define BOARD_GPIO_PWR_USB	11
+#undef  BOARD_GPIO_PWR_USB2	//9
+#define BOARD_GPIO_PWR_USB_ON	1
+#define BOARD_HAS_5G_11AC	0
+#define BOARD_NUM_ANT_5G_TX	2
+#define BOARD_NUM_ANT_5G_RX	2
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	0
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
+#define BOARD_NUM_UPHY_USB3	1
+#define BOARD_USB_PORT_SWAP	0
