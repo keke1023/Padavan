88c89bbaafde41c83e5924e1a31d2d4f877bad0e
vb1980 <vb1980@sina.com>
2022-06-28 19:26:58 +0800

    upgrade libcurl, libsqlite, libxml2

---
+++ trunk/libs/libxml2/libxml2-2.9.14/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/opt/rt-n56u/trunk/stage/lib"
+XML2_LIBS="-lxml2 -L/opt/rt-n56u/trunk/stage/lib -lz  -lpthread   -lm "
+XML2_INCLUDEDIR="-I/opt/rt-n56u/trunk/stage/include/libxml2"
+MODULE_VERSION="xml2-2.9.14"
+
