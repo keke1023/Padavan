5d746a3a8d119b46b8784ce933a8f8e1f97f1f67
vb1980 <vb1980@sina.com>
2024-02-08 09:12:01 +0800

    ipset: upgrade to 7.17

--- trunk/user/ipset/ipset-7.11/tests/sendip.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Save lineno for checking
-wc -l /var/log/kern.log | cut -d ' ' -f 1 > .loglines
-sendip "$@"
-
--- trunk/user/ipset/ipset-7.11/tests/sort.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-sed '/Members:/q' $1 > .foo
-awk '/Members:/,EOF' $1 | grep -v 'Members:' | sort >> .foo
-rm -f $1
